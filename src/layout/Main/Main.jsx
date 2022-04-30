db59a69ccc55fdb2d8976efaf10bec6384c32538
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-30 19:36:45 +0530

    main component created

---
+++ src/layout/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Auth } from "../../pages";
+
+export const Main = () => {
+  return (
+    <div className="mt-14 h-screen overflow-hidden">
+      {/* Main */}
+      <Auth />
+    </div>
+  );
+};
