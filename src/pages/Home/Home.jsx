2d4633cc3b4ca97f7fb77910742196eecad009b6
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-30 19:38:06 +0530

    home page created

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>Logged in succesfully</div>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { Auth } from "./Auth/Auth";
+export { Home } from "./Home/Home";
