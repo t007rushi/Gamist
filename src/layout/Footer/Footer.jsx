e38f730ab4a87c1e0fbb1e58ab787c6fa16e4a24
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-05-01 22:43:33 +0530

    Footer with icons and links

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
+
 export const Footer = () => {
   return (
-    <div className="fixed bottom-0 bg-slate-800 w-full h-8 text-gray-50">Footer</div>
+    <footer  className="fixed bottom-0 bg-slate-800 w-full h-8 text-gray-50">
+        <section className=" flex justify-center items-center gap-4 font-bold">
+          <a
+            href="https://twitter.com/Neo_MonkStar"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-twitter footer-icon"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/rushikeshtarapure/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-linkedin-in footer-icon"></i>
+          </a>
+          <a
+            href="https://github.com/t007rushi/Gamist"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-github footer-icon"></i>
+          </a>
+        </section>
+      </footer>
   )
 }
