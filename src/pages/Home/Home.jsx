fc19910ed782e715c581966c5be9a0573cd6a441
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-05-02 16:35:51 +0530

    Home page authorized with user data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import { useAuth } from "../../context/auth-context";
 
 export const Home = () => {
+  
+  const {userData} = useAuth();
+
   return (
-    <div>Authorized in succesfully</div>
-  )
-}
+    <div>
+      {userData.displayName} you are
+      Authorized in succesfully and {userData.email} is verified
+    </div>
+  );
+};
