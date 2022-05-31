a5c2739bc855c28452247b46895cf064861450fb
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-05-31 21:47:20 +0530

    added flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    // require("flowbite/plugin")
+  ],
+  darkMode: "class",
+};
