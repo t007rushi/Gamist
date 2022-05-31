191a18c8ed1ddda689d0b729447b9c808aa8dfc6
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-05-31 21:47:39 +0530

    isLogged check added

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   username: null,
   email: null,
+  isLoggedIn: false,
 };
 
 const authSlice = createSlice({
@@ -13,16 +14,19 @@ const authSlice = createSlice({
     userLogout: (state) => {
       state.username = null;
       state.email = null;
+      state.isLoggedIn =  false;
     },
     // login
     userLogin: (state, action) => {
       state.username = action.payload.name;
       state.email = action.payload.email;
+      state.isLoggedIn =  true;
     },
     //signup
     userSignup: (state, action) => {
       state.username = action.payload.name;
       state.email = action.payload.email;
+      state.isLoggedIn =  true;
     },
   },
 });
--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import { app } from "../../firbaseConfig";
 import { signOut, getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { userLogout } from "../../features/auth/authSlice";
 
 export const Navbar = () => {
   const auth = getAuth();
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
+  const { isLoggedIn } = useSelector((state) => state.auth);
+ 
   const signmeout = () => {
     signOut(auth)
       .then(() => {
@@ -24,15 +25,26 @@ export const Navbar = () => {
   };
   return (
     <div className="fixed top-0 z-10 bg-slate-800 w-full h-14 text-gray-50 flex flex-row items-center justify-between">
-      <h1>GAMIST</h1>
-      <div>
-        <button className="border-white border-2 p-1">Profile</button>
-        <button className="border-white border-2 p-1">Create</button>
-        <button className="border-white border-2 p-1">MSG</button>
-        <button className="border-white border-2 p-1" onClick={signmeout}>
-          Signout
-        </button>
-      </div>
+      <h1 className="font-bold text-2xl">GAMIST</h1>
+      {isLoggedIn && (
+        <div className="flex items-center">
+          <button className="p-2 m-2 text-2xl" onClick={() => navigate("/")}>
+            <i className="fa-solid fa-house"></i>
+          </button>
+          <button title="message" className="p-2 m-2 text-2xl">
+            <i className="fa-solid fa-message"></i>
+          </button>
+          <button className="p-2 m-2 text-2xl">
+            <i className="fa-solid fa-square-plus"></i>
+          </button>
+          <button className=" p-2 text-2xl">
+            <i className="fa-solid fa-user"></i>
+          </button>
+          <button className="p-2 m-2 text-2xl" onClick={signmeout}>
+            <i className="fa-solid fa-arrow-right-from-bracket"></i>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
