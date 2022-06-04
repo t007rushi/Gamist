a6215d6f2fa6107510d8c86dc7345579f03428bb
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-04 19:45:39 +0530

    users list fetching

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,17 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CreatePost } from "../../components/CreatePost";
 import { PostCard } from "../../components/PostCard";
+import { getAllUsers } from "../../features/auth/authSlice";
 import { getAllPosts } from "../../features/posts/postSlice";
 
 export const Home = () => {
   const dispatch = useDispatch();
   const { posts } = useSelector((state) => state.posts);
+  const { users } = useSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(getAllPosts());
+    dispatch(getAllUsers());
   }, []);
   return (
     <div className="flex justify-center gap-4 text-gray-900 mt-20">
@@ -34,75 +37,31 @@ export const Home = () => {
             <h5 className=" font-bold leading-none  dark:text-white">
               Suggestions for you
             </h5>
-            <a
-              href="_blank"
-              className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-            >
+            <button className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
               View all
-            </a>
+            </button>
           </div>
 
           <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-            <li className="py-2">
-              <div className="flex items-center space-x-4">
-                <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                    Neil Sims
-                  </p>
-                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                    email@windster.com
-                  </p>
-                </div>
-                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                  Follow +
-                </div>
-              </div>
-            </li>
-            <li className="py-2">
-              <div className="flex items-center space-x-4">
-                <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                    Bonnie Green
-                  </p>
-                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                    email@windster.com
-                  </p>
-                </div>
-                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                  Follow +
-                </div>
-              </div>
-            </li>
-            <li className="py-2">
-              <div className="flex items-center space-x-4">
-                <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                    Michael Gough
-                  </p>
-                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                    email@windster.com
-                  </p>
-                </div>
-                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                  Follow +
-                </div>
-              </div>
-            </li>
-            <li className="py-2">
-              <div className="flex items-center">
-                <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                    Lana Byrd
-                  </p>
-                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                    email@windster.com
-                  </p>
-                </div>
-                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                  Follow +
-                </div>
-              </div>
-            </li>
+            {users.map((user) => {
+              return (
+                <li className="py-2" key={user.userId}>
+                  <div className="flex items-center space-x-4">
+                    <div className="flex-1 min-w-0">
+                      <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                        {user.firstName}
+                      </p>
+                      <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                        {user.email}
+                      </p>
+                    </div>
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white cursor-pointer border-2 rounded-lg p-1">
+                      Follow +
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
