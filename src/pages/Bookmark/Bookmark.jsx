8be6d0a4f84c7af71c34cf757296ff8d2f4417b3
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-10 01:22:37 +0530

    prettier applied

--- src/pages/Bookmark/Bookmark.jsx
+++ src/pages/Bookmark/Bookmark.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { PostCard } from '../../components/PostCard';
+import React from "react";
+import { useSelector } from "react-redux";
+import { PostCard } from "../../components/index";
 
 export const Bookmark = () => {
-    const {user:{bookmarks}} = useSelector(state => state.auth)
-    const {posts} = useSelector(state => state.posts)
-    const mybookmarked = posts?.filter(post => bookmarks?.includes(post?.postId))
+  const {
+    user: { bookmarks },
+  } = useSelector((state) => state.auth);
+  const { posts } = useSelector((state) => state.posts);
+  const mybookmarked = posts?.filter((post) =>
+    bookmarks?.includes(post?.postId)
+  );
   return (
-         <div className="flex flex-col-reverse m-auto gap-4 w-96 mb-8 mt-20">
-        {mybookmarked?.map((post) => {
-          return (
-            <PostCard {...post} postUser={post.firstName} key={post.postId} />
-          );
-        })}
+    <div className="mb-8 mt-20">
+      <h1 className="text-black text-4xl m-4 text-center">My Bookmarks</h1>
+      <div className="flex justify-center">
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-4 mb-8">
+          {mybookmarked?.map((post) => {
+            return (
+              <PostCard {...post} postUser={post.firstName} key={post.postId} />
+            );
+          })}
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
