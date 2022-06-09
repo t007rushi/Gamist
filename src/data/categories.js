e44c0df70c9b332c03bbc05cff2d0cd76a5dd720
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-10 01:20:42 +0530

    category static data

---
+++ src/data/categories.js
@@ -0,0 +1,14 @@
+export const categories = [
+  {
+    id: "1",
+    categoryName: "🔥Trending",
+  },
+  {
+    id: "2",
+    categoryName: "✨Latest",
+  },
+  {
+    id: "3",
+    categoryName: "🐼Old",
+  },
+];
