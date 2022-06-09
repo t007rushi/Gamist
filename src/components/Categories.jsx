f27a87053e1e391cbed91329a3d3087b31f38fec
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-10 01:19:23 +0530

    category sort filter added

---
+++ src/components/Categories.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { categories } from "../data/categories";
+
+export const CategoriesFilter = ({
+  currentCategory,
+  selectCategoryHandler,
+}) => {
+  return (
+    <div className="flex gap-2 mb-2 py-4">
+      {categories?.map((category) => {
+        return (
+          <label
+          key={category.id}
+            className={
+              category.categoryName === currentCategory
+                ? "p-2 text-sm text-center font-medium rounded-full border-2 shadow-md border-gray-200 text-white bg-black"
+                : "p-2 whitespace-nowrap text-sm text-center font-medium rounded-full shadow-md border-2 border-gray-200 text-gray-900 bg-white  hover:bg-gray-100 hover:text-blue-700"
+            }
+            htmlFor={category.categoryName}
+          >
+            <input
+              className="opacity-0"
+              type="radio"
+              name="categories"
+              id={category.categoryName}
+              checked={category && category.categoryName === currentCategory}
+              onChange={(e) => selectCategoryHandler(e.target.id)}
+            />
+            {category.categoryName}
+          </label>
+        );
+      })}
+    </div>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,85 +1,59 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { CreatePost } from "../../components/CreatePost";
-import { PostCard } from "../../components/PostCard";
 import {
-  followUser,
-  getAllUsers,
-  unfollowUser,
-} from "../../features/auth/authSlice";
+  PostCard,
+  CreatePost,
+  SuggestToFollow,
+  CategoriesFilter,
+} from "../../components/index";
+import { getAllUsers } from "../../features/auth/authSlice";
 import { getAllComments, getAllPosts } from "../../features/posts/postSlice";
+import { FilterHandler } from "../../utility/categoryFilterUtil";
 
 export const Home = () => {
+  const [currentCategory, setCurrentCategory] = useState("✨Latest");
   const dispatch = useDispatch();
   const { posts } = useSelector((state) => state.posts);
-  const { users, user } = useSelector((state) => state.auth);
+  const {
+    user: { following },
+  } = useSelector((state) => state.auth);
+
+  const followingsPosts = posts.filter((post) =>
+    following?.includes(post.userId)
+  );
+
+  const selectCategoryHandler = (value) => {
+    setCurrentCategory(value);
+  };
 
   useEffect(() => {
     dispatch(getAllPosts());
     dispatch(getAllUsers());
     dispatch(getAllComments());
   }, []);
+
+  let filteredPosts = FilterHandler(currentCategory, followingsPosts);
+
   return (
     <div className="flex justify-center gap-4 text-gray-900 mt-20">
       <div className="posts">
         <div className="create-post">
           <CreatePost />
         </div>
+        <CategoriesFilter
+          currentCategory={currentCategory}
+          selectCategoryHandler={selectCategoryHandler}
+        />
         <div className="posts-column flex flex-col-reverse gap-4 mb-8">
-          {posts.map((post) => {
+          {filteredPosts.map((post) => {
             return (
               <PostCard {...post} postUser={post.firstName} key={post.postId} />
             );
           })}
         </div>
       </div>
-      <div className="suggestions hidden sm:block">
-        <div className="p-2 max-w-md bg-white rounded-lg border shadow-md dark:bg-gray-800 dark:border-gray-700 sticky top-14 mt-2">
-          <div className="flex justify-between items-center mb-4 gap-1">
-            <h5 className=" font-bold leading-none  dark:text-white">
-              Suggestions for you
-            </h5>
-            <button className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-              View all
-            </button>
-          </div>
-
-          <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-            {users.map((userl) => {
-              return (
-                <li className="py-2" key={userl.userId}>
-                  <div className="flex items-center space-x-4">
-                    <div className="flex-1 min-w-0">
-                      <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                        {userl?.firstName}
-                      </p>
-                      <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                        {userl?.email}
-                      </p>
-                    </div>
-                    {!user?.following?.includes(userl.userId) ? (
-                      <div
-                        className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white cursor-pointer border-2 rounded-lg p-1"
-                        onClick={() => dispatch(followUser(userl.userId))}
-                      >
-                        Follow
-                      </div>
-                    ) : (
-                      <div
-                        className="inline-flex items-center text-base font-semibold bg-gray-800 text-white dark:text-white cursor-pointer border-2 rounded-lg p-1"
-                        onClick={() => dispatch(unfollowUser(userl.userId))}
-                      >
-                        Unfollow
-                      </div>
-                    )}
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
+      <SuggestToFollow />
     </div>
   );
 };
